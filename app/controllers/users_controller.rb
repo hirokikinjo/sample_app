cc6bc9398a50b3f579d6b347c4699772f4daa31c
hirokikinjo <hiroki@hais.co.jp>
2017-07-25 09:33:02 +0000

    chp07 finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  # ユーザー登録処理
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザー登録が完了しました！ならゆんへようこそ～"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    
+    # ユーザーパラメーターの設定
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end

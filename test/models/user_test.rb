ac359bc899e77953e1e94f9172fa42bc7739ee94
hirokikinjo <hiroki@hais.co.jp>
2017-07-27 02:53:46 +0000

    chp09 finish

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -82,4 +82,9 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+  
+  # ブラウザをまたいだログイン／ログアウトのバグをシュミレート
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end

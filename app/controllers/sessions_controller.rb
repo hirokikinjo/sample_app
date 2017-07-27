ab8d49c0a83611cf6f671997e22c91da14fc46fa
hirokikinjo <hiroki@hais.co.jp>
2017-07-27 07:35:05 +0000

    chp11 finish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,23 +2,28 @@ class SessionsController < ApplicationController
 
   def new
   end
-  
+
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # 有効化済みのユーザーのみログイン許可する／それ以外はルートパスへリダイレクト
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
-      # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
-  
+
   def destroy
-    # ユーザーがログイン済みの場合のみログアウトする
     log_out if logged_in?
     redirect_to root_url
   end

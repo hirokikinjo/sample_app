1eab23af956c8b08bc0936d619408e4370052fd5
hirokikinjo <hiroki@hais.co.jp>
2017-07-27 08:48:20 +0000

    chp12 finish

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -12,6 +12,8 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # http://rails-tutorial2-hkinjo.c9users.io:8080/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
